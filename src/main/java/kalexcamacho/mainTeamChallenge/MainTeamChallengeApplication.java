07fd32a3a43c5950223f242a640de14662e2efd4
kalexcamacho <camachokevin34@gmail.com>
2024-01-28 09:53:30 -0500

    adding new repository first config

---
+++ src/main/java/kalexcamacho/mainTeamChallenge/MainTeamChallengeApplication.java
@@ -0,0 +1,13 @@
+package kalexcamacho.mainTeamChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainTeamChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainTeamChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kalexcamacho/mainTeamChallenge/MainTeamChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package kalexcamacho.mainTeamChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTeamChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
